68757aca8b3ff5706a3cdfc8540838d7c87fdf3c
Sgnahore <stephaniergnahore@gmail.com>
2023-07-08 15:02:09 +0100

    Fizz Buzz App NO CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
-import { greet } from "./utils/greet";
+import { useState } from "react";
+
+type MyOutputType = (number | string)[]
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
-}
 
+    const [numHiddenList, setHiddenList] = useState(1);
+    const [numListWithFB, setListWithNumFB] = useState<MyOutputType>([]); 
+  
+  
+    const Fizz = "fizz";
+    const Buzz = "buzz";
+    const FB = Fizz + Buzz;
+    
+  
+    const handleAddOne = () => {
+      setHiddenList(numHiddenList + 1);
+      setListWithNumFB([...numListWithFB, numHiddenList]);
+  
+      if (numHiddenList % 3 === 0 && numHiddenList % 5 === 0) {
+        setListWithNumFB([...numListWithFB, FB]);
+      }
+  
+      
+      else if (numHiddenList % 3 === 0) {
+        setListWithNumFB([...numListWithFB, Fizz]);
+      }
+  
+     else if (numHiddenList % 5 === 0) {
+        setListWithNumFB([...numListWithFB, Buzz]);
+      }
+    };
+  
+    const listOfNumbers = (
+      <ul>
+        {numListWithFB.map((numListWithFB, index) => (
+          <li key={index}>{numListWithFB}</li>
+        ))}
+      </ul>
+    );
+  
+    return (
+      <>
+      <button onClick={handleAddOne}>Next</button>
+        <p>Your stored numbers: {listOfNumbers}</p>
+  
+        
+      </>
+    );
+  }
+  
 export default App;
